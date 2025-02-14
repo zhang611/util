76cb82c38429a124669cb8a4c7fb8a6e4fd44b1e
zzc <zhangzhichao0601@163.com>
2025-02-14 10:51:11 +0800

    x

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <utility>
+
+void process(int& x) {
+    std::cout << "Lvalue processed: " << x << std::endl;
+}
+
+void process(int&& x) {
+    std::cout << "Rvalue processed: " << x << std::endl;
+}
+
+template <typename T>
+void forwarder(T&& x) {
+    process(std::forward<T>(x)); // 完美转发到 process
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
-    for (int i = 0; i < 10; i++) {
-        std::cout << i << std::endl;
-        
-    }
-    return 0;
+    int a = 5;
+    forwarder(a);        // 传递左值
+    forwarder(10);       // 传递右值
 }
